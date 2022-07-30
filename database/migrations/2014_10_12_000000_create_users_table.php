57cd5c0979af765d17a6060caa18df1b84160811
MD. Nazmul Hasan Anik <anikology@gmail.com>
2022-07-30 21:39:54 +0600

    multi authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->tinyInteger('role_as')->default('0'); //Add in UserTable before timestamps
             $table->timestamps();
         });
     }
